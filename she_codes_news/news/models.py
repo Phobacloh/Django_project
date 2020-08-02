c9441a9c3f43451946f39603f005c962fa46ea23
Phobacloh <anna.lowery@hotmail.com>
2020-08-02 13:48:35 +0800

    Styled the form

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,4 +6,4 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
-    image_url = models.URLField(max_length=400,default="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wired.com%2Fstory%2Fcats-australia-bushfires%2F&psig=AOvVaw0dcvs9cBr93XtPoiVwHnVo&ust=1596340320650000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPitg6CN-eoCFQAAAAAdAAAAABAD")
+    image_url = models.URLField(max_length=400,default="Paste image URL here")
