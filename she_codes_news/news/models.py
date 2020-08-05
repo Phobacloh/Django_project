d3f235c3743d566e71bbcbc564b598b74ac71e40
Phobacloh <anna.lowery@hotmail.com>
2020-08-05 19:49:00 +0800

    updated forms to have logged in user as author

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
     image_url = models.URLField(max_length=400,default="Paste image URL here")
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
