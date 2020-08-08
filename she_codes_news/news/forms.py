f2053b148f4ad29221140aaa4d10b0d500c43d73
Phobacloh <anna.lowery@hotmail.com>
2020-08-08 10:16:43 +0800

    Fixed the placeholder text and attrs in forms

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -8,13 +8,34 @@ class StoryForm(ModelForm):
         model = NewsStory
         fields = ['title', 'pub_date', 'content','image_url']
         widgets = {
+            'title':forms.TextInput(
+                attrs={
+                    'class':'label',
+                    'placeholder':'Name your masterpiece',
+                }
+            ),
+
+            'content':forms.Textarea(
+                attrs={
+                    'class':'label',
+                    'placeholder':'Make ART!',
+                }
+            ),
+                    
+            'image_url':forms.URLInput(
+                attrs={
+                    'class':'label',
+                    'placeholder':'Image URL',
+                }
+            ),
+
             'pub_date':forms.DateInput(
                 format=('%m/%d/%Y'),
                 attrs={
                     'class': 'form-control',
                     'placeholder':'Select a date',
                     'type':'date',
-                    
                 }
             )
+        
         }
